74055588bd310ddd1d4db079e7f53c2771f20ca2
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-12 14:52:38 -0300

    Updated themes

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,12 +1,20 @@
-const toggleSwitch = document.querySelector('.toggle-btn input[type="checkbox"]');
-const $html = document.querySelector('html')
+const themeButton = document.getElementById("theme-button");
+var link = document.querySelector("link[rel~='icon']");
+const $html = document.querySelector("html");
+
+const selectedTheme = localStorage.getItem("selected-theme");
 
 function darkmode() {
-    if (toggleSwitch.checked) {
-        $html.classList.toggle("dark-mode");
-        document.getElementById("banner-logo").src="images/logo_aqua.svg";
-    } else {
-        $html.classList.toggle("dark-mode");
-        document.getElementById("banner-logo").src="images/logo_flame.svg";
-    }
+  if (themeButton.classList.contains("fa-fire")) {
+    document.getElementById("banner-logo").src = "images/logo_aqua.svg";
+    link.href = "images/favicon_aqua.ico";
+    localStorage.setItem("selected-theme", "aqua");
+  } else {
+    document.getElementById("banner-logo").src = "images/logo_flame.svg";
+    link.href = "images/favicon_flame.ico";
+    localStorage.setItem("selected-theme", "flame");
+  }
+  themeButton.classList.toggle("fa-fire");
+  themeButton.classList.toggle("fa-water");
+  $html.classList.toggle("dark-mode");
 }
