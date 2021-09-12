6135752776dc6a3d07a5670d0c312d9e2360826d
Vitor Stahelin <vitorjosestahelin@gmail.com>
2021-09-12 14:59:01 -0300

    dark mode session storage

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,12 +1,38 @@
 const toggleSwitch = document.querySelector('.toggle-btn input[type="checkbox"]');
 const $html = document.querySelector('html')
 
-function darkmode() {
-    if (toggleSwitch.checked) {
-        $html.classList.toggle("dark-mode");
-        document.getElementById("banner-logo").src="images/logo_aqua.svg";
+function darkMode(value) {
+    if (value === "true") {
+        $html.classList.toggle("dark-mode", true);
+        document.getElementById("banner-logo").src = "images/logo_aqua.svg";
+        sessionStorage.setItem("dark-mode", "true");
     } else {
-        $html.classList.toggle("dark-mode");
-        document.getElementById("banner-logo").src="images/logo_flame.svg";
+        $html.classList.toggle("dark-mode", false);
+        document.getElementById("banner-logo").src = "images/logo_flame.svg";
+        sessionStorage.setItem("dark-mode", "false");
+    }
+}
+
+function toggleSwitchDarkMode() {
+    if (toggleSwitch.checked) {
+        darkMode("true")
+        console.log(sessionStorage)
+    } else {
+        darkMode("false")
+        console.log(sessionStorage)
+    }
+}
+
+window.onload = function () {
+    var dark_mode = sessionStorage.getItem("dark-mode");
+    console.log(dark_mode)
+    if (dark_mode === "true") {
+        console.log("n ta falso esse kct")
+        document.getElementById("dark-mode-check-box").checked = true;
+        darkMode("true")
+    } else {
+        console.log("ta falso esse kct")
+        document.getElementById("dark-mode-check-box").checked = false;
+        darkMode("false")
     }
 }
